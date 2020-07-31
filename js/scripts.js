555fdd000594b27aa50d2e0da17c08b004ce2ad9
JosephJacob <josephjacob313@gmail.com>
2020-07-31 12:09:07 +0300

    add function so that when you send a message, an alert will popup with your name

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+    $(".card-1").hover(function(){
+        $(this).animate({opacity:'1'});
+    },
+    function(){4
+        $(this).animate({opacity:'0'});
+        });    
+        $('#mc-embedded-subscribe').click(function () {
+            var name = document.getElementById("mce-NAME").value;
+            alert("Hey " + name + ". We have received your details thank you for reaching us!");
+        });
+        
+})
